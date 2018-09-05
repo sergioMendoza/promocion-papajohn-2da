3ff6f00d728fae82a2b532a62498d06d99c863bb
carlacentenor <carlacentenor@gmail.com>
2018-09-05 15:58:27 -0500

    empaquetando para producción

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,16 @@ var concat = require('gulp-concat');
 var babel = require('gulp-babel');
 var minifycss = require('gulp-minify-css'); //npm i gulp-minify-css -D
 var sass = require('gulp-sass'); //npm install gulp-sass -D
-
+var imagemin = require('gulp-imagemin');
 // copiando archivos de bootstrap
 gulp.task('sass', () => {
     return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss',
             'src/sass/*.scss'
         ])
         .pipe(sass())
-        // .pipe(concat('main.min.css'))
-        // .pipe(minifycss())
-        //.pipe(gulp.dest('public/css'))
+        .pipe(concat('main.min.css'))
+        .pipe(minifycss())
+        .pipe(gulp.dest('public/css'))
 })
 
 
@@ -28,11 +28,18 @@ gulp.task('js', function() { // instalar  npm i gulp-concat gulp-uglify gulp-bab
 })
 
 
+gulp.task('images', function () {
+    return gulp.src(['src/assets/images/*.*'])
+        .pipe(imagemin())
+        .pipe(gulp.dest('public/assets/images/'));
+  });
+  
+
 // una tarea que ejecuta todo
-gulp.task('minifica-todo', ['sass', 'js'])
+gulp.task('minifica-todo', ['sass', 'js','images'])
 
 // va actualizando ante cualquier cambio
 gulp.task('watch', function() {
     gulp.watch(['src/css/*.css', 'src/sass/*.scss'], ['css']);
-    // gulp.watch('src/js/*.js', ['js']);
+    gulp.watch('src/js/*.js', ['js']);
 })
